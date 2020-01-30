ba2ad9a50836e51b623ad6be61f978cdfdb8131f
Eduardo mendoza <garibo.eduardo90@gmail.com>
2020-01-30 17:30:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,14 @@
 # Your Code Here
 def map(source_array)
-  yield(source_array.map { |x| x * })
+  new_arr = []
+  idx = 0
+  while idx < source_array.length
+    new_arr << yield(new_arr[idx])
+    i += 1
+  end
+  new_arr
+end
+
+def reduce(source_array, initial = 0)
+  
 end
